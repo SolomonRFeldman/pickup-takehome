05f4bae20026fb0c10611bde4c34fe2e35ca7ea2
Solomon Raphael Feldman <SolomonRFeldman@gmail.com>
2021-10-22 17:33:22 -0400

    adds form data with console log submitting

--- pickup-takehome-client/src/components/Pickup/CreatePickupForm.jsx
+++ pickup-takehome-client/src/components/Pickup/CreatePickupForm.jsx
@@ -3,8 +3,18 @@ import { Form, Modal, Button } from 'react-bootstrap'
 import { postRequest } from '../../apiRequests'
 
 export default function CreatePickupForm({ show, handleClose }) {
+  const [formData, setFormData] = useState({ 
+    address: { street_address: '', street_address_line2: '', city: '', state: '', zip: '' },
+    date: '',
+    status: 'pending'
+  })
+  const handleChange = event => setFormData({ ...formData, [event.target.id]: event.target.value })
+  const handleAddressChange = event => setFormData({ ...formData, address: { ...formData['address'], [event.target.id]: event.target.value }})
+
   const handleSubmit = event => {
     event.preventDefault()
+    console.log(formData)
+    // return postRequest('/pickups', { pickup: formData })
   }
 
   return(
@@ -14,6 +24,30 @@ export default function CreatePickupForm({ show, handleClose }) {
       </Modal.Header>
       <Form onSubmit={handleSubmit}>
         <Modal.Body>
+          <Form.Group controlId='date'>
+            <Form.Label>Date</Form.Label>
+            <Form.Control type='date' onChange={handleChange} value={formData.date} />
+          </Form.Group>
+          <Form.Group controlId='street_address'>
+            <Form.Label>Street Address</Form.Label>
+            <Form.Control placeholder='Street Address' type='text' onChange={handleAddressChange} value={formData.email} />
+          </Form.Group>
+          <Form.Group controlId='street_address_line2'>
+            <Form.Label>Street Address Line 2</Form.Label>
+            <Form.Control placeholder='Street Address Line 2' type='text' onChange={handleAddressChange} value={formData.email} />
+          </Form.Group>
+          <Form.Group controlId='city'>
+            <Form.Label>City</Form.Label>
+            <Form.Control placeholder='City' type='text' onChange={handleAddressChange} value={formData.email} />
+          </Form.Group>
+          <Form.Group controlId='state'>
+            <Form.Label>State</Form.Label>
+            <Form.Control placeholder='State' type='text' onChange={handleAddressChange} value={formData.email} />
+          </Form.Group>
+          <Form.Group controlId='zip'>
+            <Form.Label>Zip</Form.Label>
+            <Form.Control placeholder='Zip' type='text' onChange={handleAddressChange} value={formData.email} />
+          </Form.Group>
         </Modal.Body>
         <Modal.Footer>
           <Button aria-label='Create Pickup' type='submit'>Create Pickup</Button>
